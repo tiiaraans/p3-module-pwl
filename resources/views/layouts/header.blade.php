ee214e938d23f642e97b9263171e87c9d769dfaa
tiiaraans <14tiaraannisa@gmail.com>
2022-03-09 02:21:21 +0700

    tambahan halaman about us

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,70 @@
+<div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <!-- <a class="navbar-brand" href="index.html">Bootstrap Restaurant</a> -->
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <!-- <li class="active"><a href="index.html">Home</a></li> -->
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="#tablebooking">Table Booking</a></li>
+				<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Indina <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">DRINKS</a></li>
+                    <li><a href="#">STARTERS</a></li>
+                    <li><a href="#">TANDOORI - CLAY OVEN - DISHES</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">SEAFOOD MAIN COURSES</li>
+                    <li><a href="#">CHICKEN MAIN COURSES</a></li>
+                    <li><a href="#">LAMB MAIN COURSES</a></li>
+                    <li><a href="#">RICE/BREDS</a></li>
+                    <li><a href="#">ACCOMPANIMENTS</a></li>
+                  </ul>
+                </li>
+				
+				<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Italian <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">DRINKS</a></li>
+                    <li><a href="#">STARTERS</a></li>
+                    <li><a href="#">TANDOORI - CLAY OVEN - DISHES</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">SEAFOOD MAIN COURSES</li>
+                    <li><a href="#">CHICKEN MAIN COURSES</a></li>
+                    <li><a href="#">LAMB MAIN COURSES</a></li>
+                    <li><a href="#">RICE/BREDS</a></li>
+                    <li><a href="#">ACCOMPANIMENTS</a></li>
+                  </ul>
+                </li>
+				
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Chines <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">DRINKS</a></li>
+                    <li><a href="#">STARTERS</a></li>
+                    <li><a href="#">TANDOORI - CLAY OVEN - DISHES</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">SEAFOOD MAIN COURSES</li>
+                    <li><a href="#">CHICKEN MAIN COURSES</a></li>
+                    <li><a href="#">LAMB MAIN COURSES</a></li>
+                    <li><a href="#">RICE/BREDS</a></li>
+                    <li><a href="#">ACCOMPANIMENTS</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'page.home');
+Route::view('/about', 'page.about');
